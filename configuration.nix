d2a91ce125336b3c7933d282c33e2e1fbdfeee48
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2018-08-29 08:48:47 -0600

    break out bootloader config

--- configuration.nix
+++ configuration.nix
@@ -8,13 +8,10 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./bootloader.nix
     ];
 
   nixpkgs.config.allowUnfree = true;
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
