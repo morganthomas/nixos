c2b39f895c8ce904c4942e39d5db82c77d3c4017
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-06-23 00:32:41 -0600

    Configure for Obelisk

--- configuration.nix
+++ configuration.nix
@@ -100,4 +100,7 @@
   };
 
   networking.timeServers = options.networking.timeServers.default;
+  
+  nix.binaryCaches = [ "https://nixcache.reflex-frp.org" ];
+  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
 }
