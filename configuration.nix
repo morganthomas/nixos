041904606e1939f6289ce0a1a8cb7e06f985b72a
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-09 23:39:42 -0600

    Trust NixOS and Shpadoinkle binary caches

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,6 @@
 
   networking.timeServers = options.networking.timeServers.default;
   
-  nix.binaryCaches = [ "https://nixcache.reflex-frp.org" ];
-  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
+  nix.binaryCaches = [ "https://nixcache.reflex-frp.org" "https://cache.nixos.org" "https://shpadoinkle.cachix.org" ];
+  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "shpadoinkle.cachix.org-1:aRltE7Yto3ArhZyVjsyqWh1hmcCf27pYSmO1dPaadZ8=" ];
 }
