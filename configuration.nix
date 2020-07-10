a47018877a2086ef04d003a49d3b136077f9c0ca
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-10 15:10:40 -0600

    Add wifi credentials

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,7 @@
 
   # networking.hostName = "nixos"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.networks = { anobjectis = { psk = "aconcretemonoid"; }; };
 
   # Select internationalisation properties.
   # i18n = {
