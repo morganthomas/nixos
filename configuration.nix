00cddd470e86c1736b361e1e6f80e1be9b840394
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-23 13:03:14 -0600

    add docker

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,7 @@
   ];
 
   services.mongodb.enable = true;
+  virtualisation.docker.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
