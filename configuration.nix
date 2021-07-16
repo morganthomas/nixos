10797ad611f12953e05a65f13fd0690206f097be
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-07-16 10:39:37 -0400

    simplify

--- configuration.nix
+++ configuration.nix
@@ -31,37 +31,20 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget chromium git tmate wdiff psmisc zip nix-prefetch-git vim
-    gnumake gcc binutils-unwrapped ncurses5 zlib.dev scrot inkscape nodePackages.node2nix
-    gnupg dos2unix nix-serve usbutils xmobar bpytop fd tilix dmenu networkmanager
-    mkpasswd zip unzip openfortivpn i7z nginx iftop ardour ffmpeg jq
-    youtube-dl vlc awscli ghc patchelf stack vscode pavucontrol htop
-    haskell.compiler.ghc865 gimp nodejs signal-desktop haskellPackages.haskell-language-server
-    wavemon cdparanoia cdrdao bind google-chrome phantomjs2 libreoffice adoptopenjdk-bin
-    cdrkit dvdplusrwtools
+    gnumake gcc binutils-unwrapped ncurses5 zlib.dev scrot
+    gnupg dos2unix xmobar fd tilix dmenu networkmanager
+    mkpasswd zip unzip i7z jq htop discord
   ];
 
   services.ntp.enable = true;
-  virtualisation.virtualbox.host.enable = true;
+  programs.dconf.enable = true;
   users.extraGroups.vboxusers.members = [ "morgan" ];
-
-  hardware.pulseaudio = {
-    enable = true;
-    package = pkgs.pulseaudioFull;
-    zeroconf.discovery.enable = true;
-  };
-  services.printing.enable = true;
-  services.printing.drivers = [ pkgs.gutenprint pkgs.brlaser ];
-  services.mongodb.enable = true;
-  virtualisation.docker.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  programs.ssh.startAgent = true;
-  programs.dconf.enable = true;
 
   # List services that you want to enable:
 
@@ -83,7 +66,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.wacom.enable = true;
   services.xserver.windowManager.xmonad = {
     enable = true;
     enableContribAndExtras = true;
@@ -91,35 +73,6 @@
   services.xserver.xkbOptions = "ctrl:swapcaps";
   services.xserver.videoDrivers = [ "intel" ];
   # services.xserver.xkbOptions = "eurosign:e";
-
-  systemd.services.mute = with pkgs; {
-    serviceConfig.type = "oneshot";
-    script = ''
-      ${alsaUtils}/bin/amixer sset Master mute
-    '';
-  };
-
-  systemd.services.unmute = with pkgs; {
-    serviceConfig.type = "oneshot";
-    script = ''
-      ${alsaUtils}/bin/amixer sset Master unmute
-    '';
-  };
-
-  systemd.timers.mute = {
-    wantedBy = [ "timers.target" ];
-    partOf = [ "mute.service" ];
-    timerConfig.OnCalendar = "*-*-* 00:00:00";
-    timerConfig.Persistent = true;
-  };
-
-  systemd.timers.unmute = {
-    wantedBy = [ "timers.target" ];
-    partOf = [ "unmute.service" ];
-    timerConfig.OnCalendar = "*-*-* 09:00:00";
-    timerConfig.Persistent = true;
-  };
-
   # Enable touchpad support.
   # services.xserver.libinput.enable = true;
 
@@ -140,35 +93,11 @@
   # should.
   system.stateVersion = "20.09"; # Did you read the comment?
   system.autoUpgrade.enable = true;
-
-  fileSystems = {
-    "/home/morgan/media/SECURE_KEY" = {
-      device = "/dev/disk/by-uuid/C470-4BC9";
-      fsType = "vfat";
-      options = [ "noauto" ];
-    };
-  };
-  
-  systemd.automounts = [
-    { where = "/home/morgan/media/SECURE_KEY";
-     wantedBy = [ "default.target" ]; }
-  ];
-  
+ 
   swapDevices = [{ device = "/swapfile"; }];
 
   networking.timeServers = options.networking.timeServers.default;
   
   nix.binaryCaches = [ "https://nixcache.reflex-frp.org" "https://cache.nixos.org" "https://shpadoinkle.cachix.org https://hydra.iohk.io" ];
   nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "shpadoinkle.cachix.org-1:aRltE7Yto3ArhZyVjsyqWh1hmcCf27pYSmO1dPaadZ8= iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_12;
-    authentication = pkgs.lib.mkForce ''
-    local all all trust
-    host  all all 127.0.0.1/32 trust
-    host  all all ::1/128      trust
-    host  all all 0.0.0.0/0    trust
-    '';
-  };
 }
