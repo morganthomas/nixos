333884f0fa4f01a0b18e9b3c8afdbab7079c91f8
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-02-11 11:26:21 -0500

    misc updates

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,8 @@
       ./hostname.nix
     ];
 
+  nix.trustedUsers = [ "root" "morgan" ];
+
   nixpkgs.config.allowUnfree = true;
 
   networking.networkmanager.enable = true;
@@ -33,7 +35,7 @@
     gnumake gcc binutils-unwrapped ncurses5 zlib.dev scrot
     gnupg dos2unix xmobar fd tilix dmenu networkmanager
     mkpasswd zip unzip i7z jq htop discord xscreensaver
-    inkscape nodejs-12_x python39 vlc
+    inkscape nodejs-12_x python39 vlc file rustup
   ];
 
   #services.ntp.enable = true;
@@ -127,7 +129,6 @@
       "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
       "shpadoinkle.cachix.org-1:aRltE7Yto3ArhZyVjsyqWh1hmcCf27pYSmO1dPaadZ8="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "ci.ardana.platonic.systems:yByqhxfJ9KIUOyiCe3FYhV7GMysJSA3i5JRvgPuySsI="
       "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
     ];
     binaryCaches = [
@@ -135,7 +136,6 @@
       "https://nixcache.reflex-frp.org"
       "https://shpadoinkle.cachix.org"
       "https://hydra.iohk.io" 
-      "ssh://nix-ssh@ci.ardana.platonic.systems?ssh-key=/home/morgan/.ssh/id_ed25519"
     ];
   };
 }
